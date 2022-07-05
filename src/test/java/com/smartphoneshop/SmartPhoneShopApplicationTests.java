2bdeb51f721937ec2ea0e6dae112174806933c55
= <birtday11111@gmail.com>
2022-07-05 23:44:59 +0700

    first commit

---
+++ src/test/java/com/smartphoneshop/SmartPhoneShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartphoneshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPhoneShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
